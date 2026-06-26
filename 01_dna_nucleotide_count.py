2262a92869848e7595fb5e59aab4afcad2ede285
tmiyatov-maker <t.miyatov@gmail.com>
2026-06-26 19:19:40 -0400

    Add DNA nucleotide counting script
    This script counts the occurrences of A, C, G, and T nucleotides in a given DNA sequence.

---
+++ 01_dna_nucleotide_count.py
@@ -0,0 +1,22 @@
+"""
+Problem: DNA Nucleotide Count
+
+Description:
+Counts the number of A, C, G, and T nucleotides in a DNA sequence.
+
+Concepts:
+- Strings
+- .count()
+- Variables
+
+Author: Temirlan Miyatov
+"""
+
+dna = "ATCGATCGA"
+
+print(
+    dna.count("A"),
+    dna.count("C"),
+    dna.count("G"),
+    dna.count("T")
+)
