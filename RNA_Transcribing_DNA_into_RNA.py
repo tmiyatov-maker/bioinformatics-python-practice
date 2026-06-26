1dc277b970fbe0a279a3700f5fc0ff831c7246a6
tmiyatov-maker <t.miyatov@gmail.com>
2026-06-26 19:27:34 -0400

    Revise RNA transcription problem details
    Converting dna into rna

---
+++ RNA_Transcribing_DNA_into_RNA.py
@@ -0,0 +1,16 @@
+"""
+Rosalind Problem: RNA - Transcribing DNA into RNA
+
+Description:
+Converts DNA into RNA by replacing T with U.
+
+Concepts:
+- String replacement
+- Transcription
+"""
+
+dna = "GATGGAACTTGACTACGTAAATT"
+
+rna = dna.replace("T", "U")
+
+print(rna)
